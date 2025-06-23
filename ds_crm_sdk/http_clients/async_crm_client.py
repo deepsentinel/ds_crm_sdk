7776a457ce9c1bb38b76acd646390b24b80e9768
prav2019 <praveen@deepsentinel.com>
2025-06-23 14:56:23 -0700

    change the return types for the client along with their doc

--- ds_crm_sdk/http_clients/async_crm_client.py
+++ ds_crm_sdk/http_clients/async_crm_client.py
@@ -2,7 +2,6 @@ from ds_crm_sdk.transports.base import HTTPMethod, AsyncHTTPTransport
 from ds_crm_sdk.payloads import MainPayloadBuilder
 from .endpoints import AccountEndpoints
 
-# Todo: Return data will be different, work on that next
 
 class AsyncCRMClient:
     def __init__(self, base_url: str, client_type: str, transport: AsyncHTTPTransport, builder: MainPayloadBuilder):
@@ -13,78 +12,83 @@ class AsyncCRMClient:
         # Creates the main payload based on the client type (skeleton)
         self.__main_payload = self.__builder.build_main_payload(client_type=self.__client_type)
 
-    async def get_account(self, account_id: str):
+    async def get_account(self, account_id: str) -> tuple:
         """
         Get account details by account ID.
         :param account_id: The ID of the account to retrieve.
-        :return: Account details as a dictionary.
+        :return: Account details as a dictionary with http status code.
         """
         endpoint = AccountEndpoints.SPECIFIC_ACCOUNT.format(account_id=account_id)
-        return await self.__transport.send(
+        data, status_code = await self.__transport.send(
             method=HTTPMethod.GET,
             endpoint=self.__base_url + endpoint,
             params=self.__main_payload.dict()
         )
+        return data, status_code
 
-    async def get_accounts_by_filters(self, filters: dict, page=1, page_size=10):
+    async def get_accounts_by_filters(self, filters: dict, page=1, page_size=10) -> tuple:
         """
         Get accounts by applying filters.
         :param filters: A dictionary containing the filters to apply.
         :param page: The page number for pagination.
         :param page_size: The number of accounts per page for pagination.
-        :return: A list of accounts matching the filters.
+        :return: A list of accounts matching the filters with http status code.
         """
         endpoint = AccountEndpoints.BASE
         params = {**self.__main_payload.dict(), **filters, 'page': page, 'page_size': page_size}
-        return await self.__transport.send(
+        data, status_code = await self.__transport.send(
             method=HTTPMethod.GET,
             endpoint=self.__base_url + endpoint,
             params=params
         )
+        return data, status_code
 
-    async def get_account_addresses(self, account_id: str, page=1, page_size=10):
+    async def get_account_addresses(self, account_id: str, page=1, page_size=10) -> tuple:
         """
         Get addresses for a specific account.
         :param account_id: The ID of the account whose addresses are to be retrieved.
         :param page: The page number for pagination.
         :param page_size: The number of addresses per page for pagination.
-        :return: A list of addresses associated with the account.
+        :return: A list of addresses associated with the account with http status code.
         """
         endpoint = AccountEndpoints.ACCOUNT_ADDRESSES.format(account_id=account_id)
         params = {**self.__main_payload.dict(), 'page': page, 'page_size': page_size}
-        return await self.__transport.send(
+        data, status_code = await self.__transport.send(
             method=HTTPMethod.GET,
             endpoint=self.__base_url + endpoint,
             params=params)
+        return data, status_code
 
-    async def get_account_addresses_by_filters(self, account_id: str, filters: dict, page=1, page_size=10):
+    async def get_account_addresses_by_filters(self, account_id: str, filters: dict, page=1, page_size=10) -> tuple:
         """
         Get addresses for a specific account by applying filters.
         :param account_id: The ID of the account whose addresses are to be retrieved.
         :param filters: A dictionary containing the filters to apply.
         :param page: The page number for pagination.
         :param page_size: The number of addresses per page for pagination.
-        :return: A list of addresses associated with the account matching the filters.
+        :return: A list of addresses associated with the account matching the filters with http status code.
         """
         endpoint = AccountEndpoints.ACCOUNT_ADDRESSES.format(account_id=account_id)
         params = {**self.__main_payload.dict(), **filters, 'page': page, 'page_size': page_size}
-        return await self.__transport.send(
+        data, status_code = await self.__transport.send(
             method=HTTPMethod.GET,
             endpoint=self.__base_url + endpoint,
             params=params
         )
+        return data, status_code
 
-    async def get_account_types(self, page=1, page_size=10):
+    async def get_account_types(self, page=1, page_size=10) -> tuple:
         """
         Get all account types.
         :param page: The page number for pagination.
         :param page_size: The number of account types per page for pagination.
-        :return: A list of account types.
+        :return: A list of account types with http status code.
         """
         endpoint = AccountEndpoints.ACCOUNT_TYPES
         params = {**self.__main_payload.dict(), 'page': page, 'page_size': page_size}
-        return await self.__transport.send(
+        data, status_code = await self.__transport.send(
             method=HTTPMethod.GET,
             endpoint=self.__base_url + endpoint,
             params=params
         )
+        return data, status_code
