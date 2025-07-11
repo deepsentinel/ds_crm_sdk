2c8891a43a58439763f536a3b7b4b6dbbe34365c
prav2019 <praveen@deepsentinel.com>
2025-07-11 13:21:51 -0700

    update account request DTO

--- ds_crm_sdk/dtos/account.py
+++ ds_crm_sdk/dtos/account.py
@@ -1,20 +1,22 @@
 from pydantic import BaseModel
-
+from typing import Optional
 
 
 class AccountRequestDTO(BaseModel):
     """
     DTO for building account requests in the CRM system.
     """
-    account_type_id: int = None
-    parent_account_id: int = None
-    name: str = None
-    phone_number: str = None
-    email_address: str = None
-    created_by: str = None
-    # This field is used to associate the account with a user in the system (account_user)
-    user_id: str = None
-    # This field is used to check if the account already exists, if it does, then this is just an association request,
-    # no new account will be created.
-    existing_account_email: str = None
+    account_type_id: Optional[int] = None
+    parent_account_id: Optional[int] = None
+    name: Optional[str] = None
+    description: Optional[str] = None
+    phone_number: Optional[str] = None
+    email_address: Optional[str] = None
+    created_by: str
+    user_id: str
+    # First name and last name are used to create name and description of the account, so required for now !!!
+    first_name: str
+    last_name: str
+    existing_account_email: Optional[str] = None
+
 
